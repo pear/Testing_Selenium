a5b83c976f5b5d9ff32396dcceb619da126f2ce7
Shin Ohno <shin@php.net>
2006-10-12 17:02:30 +0000

    Added Bug TestCase
    git-svn-id: http://svn.php.net/repository/pear/packages/Testing_Selenium/trunk@221634 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/Bug8891.php
@@ -0,0 +1,59 @@
+<?php
+require_once '../Selenium.php';
+require_once 'PHPUnit2/Framework/TestCase.php';
+
+class Bug8891 extends PHPUnit2_Framework_TestCase
+{
+    private $selenium;
+
+    public function __construct($name)
+    {
+        $this->browserUrl = "http://www.unchiku.com/";
+        $this->testUrl = $this->browserUrl . "tests";
+        parent::__construct($name);
+    }
+// {{{ setUp and tearDown
+    public function setUp()
+    {
+        try {
+            $this->selenium = new Selenium("*firefox", $this->browserUrl);
+            $this->selenium->start();
+        } catch (Selenium_Exception $e) {
+            $this->selenium->stop();
+            echo $e;
+        }
+    }
+
+    public function tearDown()
+    {
+        try {
+           $this->selenium->stop();
+        } catch (Selenium_Exception $e) {
+            echo $e;
+        }
+    }
+    // }}}
+    // {{{ testOpen
+    public function test8893()
+    {
+        try {
+            $this->selenium->open("{$this->testUrl}/html/test_open.html");
+
+            // The default is glob
+            $this->assertFalse($this->selenium->isTextPresent('Foo'));
+            $this->assertTrue($this->selenium->isTextPresent('This*'));
+            $this->assertTrue($this->selenium->isTextPresent('This?is'));
+            $this->assertTrue($this->selenium->isTextPresent('T?????'));
+
+            // To use JavaScrpt regexp, speicify regexp: as follows:
+            $this->assertTrue($this->selenium->isTextPresent('regexp:^This'));
+            $this->assertTrue($this->selenium->isTextPresent('regexp:^Th.s'));
+
+        } catch (Selenium_Exception $e) {
+            $this->selenium->stop();
+            echo $e;
+        }
+    }
+    // }}}
+}
+?>
---
+++ tests/Bug8893.php
@@ -0,0 +1,49 @@
+<?php
+require_once '../Selenium.php';
+require_once 'PHPUnit2/Framework/TestCase.php';
+
+class Bug8893 extends PHPUnit2_Framework_TestCase
+{
+    private $selenium;
+
+    public function __construct($name)
+    {
+        $this->browserUrl = "http://www.google.de/";
+        $this->testUrl = $this->browserUrl;
+        parent::__construct($name);
+    }
+// {{{ setUp and tearDown
+    public function setUp()
+    {
+        try {
+            $this->selenium = new Selenium("*firefox", $this->browserUrl);
+            $this->selenium->start();
+        } catch (Selenium_Exception $e) {
+            $this->selenium->stop();
+            echo $e;
+        }
+    }
+
+    public function tearDown()
+    {
+        try {
+           $this->selenium->stop();
+        } catch (Selenium_Exception $e) {
+            echo $e;
+        }
+    }
+    // }}}
+    // {{{ testOpen
+    public function test8893()
+    {
+        try {
+            $this->selenium->open("{$this->testUrl}");
+            var_dump($this->selenium->getAllButtons());
+        } catch (Selenium_Exception $e) {
+            $this->selenium->stop();
+            echo $e;
+        }
+    }
+    // }}}
+}
+?>
