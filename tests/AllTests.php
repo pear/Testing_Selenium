e1ecabbba66349471f33bd077fc1d1c73f2b543a
Shin Ohno <shin@php.net>
2006-12-08 15:35:37 +0000

    Bug8893.php Bug9119.php Bug9189.php are moved to BugTest.php (TODO Bug #9189 still remain.) Added GoogleTest.php, BugTest.php So that, AllTests.php are modified to compatible for the changes above.
    git-svn-id: http://svn.php.net/repository/pear/packages/Testing_Selenium/trunk@224626 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -1,52 +1,36 @@
 <?php
-
-error_reporting(E_ALL|E_STRICT);
-
-require_once 'PHPUnit/Util/Filter.php';
-
-PHPUnit_Util_Filter::addFileToFilter(__FILE__);
-
+// Commands are tested in Selenium RC build, and all commands are generated using XSLT.
+// So this tests only if basic commands work.
 if (!defined('PHPUnit_MAIN_METHOD')) {
-    define('PHPUnit_MAIN_METHOD', 'AllTests::main');
-    chdir(dirname(__FILE__));
+    define('PHPUnit_MAIN_METHOD', 'TestSuite::main');
 }
-
-if (!defined('PHPUnit_INSIDE_OWN_TESTSUITE')) {
-    define('PHPUnit_INSIDE_OWN_TESTSUITE', TRUE);
-}
-
-require_once 'PHPUnit/Framework/TestSuite.php';
+ 
+require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 require_once 'SeleniumTest.php';
-require_once 'Bug8891.php';
-require_once 'Bug9119.php';
-require_once 'Bug9189.php';
-//require_once 'Bug8893.php';
-
+require_once 'GoogleTest.php';
+require_once 'BugTest.php';
+ 
 class AllTests
 {
     public static function main()
     {
-
         PHPUnit_TextUI_TestRunner::run(self::suite());
     }
-
+ 
     public static function suite()
     {
-        $suite = new PHPUnit_Framework_TestSuite('Testing');
-        /** Add testsuites, if there is. */
+        $suite = new PHPUnit_Framework_TestSuite('PHPUnit Framework');
         $suite->addTestSuite('SeleniumTest');
-        $suite->addTestSuite('Bug8891');
-//        $suite->addTestSuite('Bug8893');
-        $suite->addTestSuite('Bug9119');
-        $suite->addTestSuite('Bug9189');
-
+        $suite->addTestSuite('GoogleTest');
+        $suite->addTestSuite('BugTest');
+ 
         return $suite;
     }
 }
-
-if (PHPUnit_MAIN_METHOD == 'AllTests::main') {
-    AllTests::main();
+ 
+if (PHPUnit_MAIN_METHOD == 'Framework_AllTests::main') {
+    Framework_AllTests::main();
 }
 ?>
